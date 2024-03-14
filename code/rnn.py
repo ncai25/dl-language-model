8d83dec06ca1b87cc6f3baf0b49f80d343537d2e
norafk <noracai0625@gmail.com>
2024-03-14 01:14:29 -0400

    fsdfsdfs

--- code/rnn.py
+++ code/rnn.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 import numpy as np
+import random
 from preprocess import get_data
 from types import SimpleNamespace
 
@@ -43,11 +44,11 @@ class MyRNN(tf.keras.Model):
         - You must use an LSTM or GRU as the next layer.
         """
         X_RNN_embedding = self.embedding_layer(inputs)
-        rnn_output = self.rnn_layer(X_RNN_embedding, initial_state = None)
+        rnn_output = self.rnn_layer(X_RNN_embedding)
         logits = self.output_layer(rnn_output)
 
-        print(f'embedding shape, {X_RNN_embedding.shape}')
-        print(f'rnn_output shape, {rnn_output.shape}')
+        # print(f'embedding shape, {X_RNN_embedding.shape}')
+        # print(f'rnn_output shape, {rnn_output.shape}')
         # [batch, timesteps, rnn_size]
 
         return logits
@@ -135,24 +136,76 @@ def main():
     ##   from train_x and test_x. You also need to drop the first element from train_y and test_y.
     ##   If you don't do this, you will see very, very small perplexities.
     ##   HINT: You might be able to find this somewhere...
-
-
     LOCAL_TRAIN_FILE = '/Users/noracai/Documents/CS1470/homework-4p-language-models-norafk/data/train.txt'
     LOCAL_TEST_FILE = '/Users/noracai/Documents/CS1470/homework-4p-language-models-norafk/data/test.txt'
     
     train_id, test_id, vocabulary = get_data(LOCAL_TRAIN_FILE, LOCAL_TEST_FILE)
+   
+    window_size = 20 
+    offset = random.randint(0, window_size - 1)
+    print(offset)
+
+# example_sentence_tokenized_array = np.array(example_sentence_tokenized)
+# remainder = (len(example_sentence_tokenized_array) - 1)%window_size
+# example_sentence_tokenized_array = example_sentence_tokenized_array[:-remainder]
+
+
+# # 2. Define X_RNN and y_RNN  
+# X_RNN = example_sentence_tokenized_array[:-1].reshape(-1, 4)
+# # -1 in reshape means whatever it takes to flatten
+# y_RNN = example_sentence_tokenized_array[1:].reshape(-1, 4)
 
     ## Process the data
-    def process_trigram_data(data):
-        X = np.array(data[:-1])
-        Y = np.array(data[2:])
-        X = np.column_stack((X[:-1], X[1:]))
+    def process_rnn_data(data):
+        offset_data = np.array(data[offset:])
+        remainder = (len(offset_data)-1)%window_size
+        X = tf.reshape(offset_data[:-remainder][:-1], (-1, window_size))
+        Y = tf.reshape(offset_data[:-remainder][1:], (-1, window_size))
+
         return X, Y
 
-    X0, Y0 = process_trigram_data(train_id)
-    X1, Y1 = process_trigram_data(test_id)
+    X0, Y0 = process_rnn_data(train_id)
+    print(f"X_RNN shape = {X0.shape}")
+    print(f"Y_RNN shape = {Y0.shape}")
+
+
+    X1, Y1 = process_rnn_data(test_id)
 
     vocab = vocabulary
+    
+    # # # Divide the offset training corpus into windows of the specified window size
+    # for i in range(0, len(X0_offset) - window_size, window_size):
+    #     window_X0 = X0_offset[i:i+window_size]
+    #     window_Y0 = Y0_offset[i:i+window_size]
+        
+
+
+    # print(X0.shape)
+
+    # remainder_x0 = (len(X0)-1)%window_size
+    # X0 = X0[:-remainder_x0]
+    # X0 = tf.reshape(X0[:-1], (-1, window_size))
+    # print(X0.shape)
+
+    # # X_RNN_embedding = tf.reshape(X_RNN_embedding, (-1, 2*self.embed_size)) # tk 
+
+    # print(Y0.shape)
+
+    # remainder_y0 = (len(Y0)-1)%window_size
+    # Y0 = Y0[:-remainder_y0]
+    # print(Y0.shape)
+
+    # Y0 = tf.reshape(Y0[1:], (-1, window_size))
+    # print(Y0.shape)
+
+    # remainder_x1 = (len(X1)-1)%window_size
+    # X1 = X1[:-remainder_x1]
+    # X1 = tf.reshape(X1[:-1], (-1, window_size))
+
+    # remainder_y1= (len(Y1)-1)%window_size
+    # Y1 = Y1[:-remainder_y1]
+    # Y1 = tf.reshape(Y1[1:], (-1, window_size))
+
 
     ## TODO: Get your model that you'd like to use
     args = get_text_model(vocab)
